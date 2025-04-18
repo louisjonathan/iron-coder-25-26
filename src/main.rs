c247dc1ac5a1919eea5a4068b350ddb2e20513cf
Evan Purutcuoglu <tarnation103@gmail.com>
2025-04-17 21:21:50 -0400

    Update main.rs
    Added minimum window size and appropriate window name.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use egui_dock_testing::app::MainWindow;
 
 #[cfg(not(target_arch = "wasm32"))]
 pub fn main() -> eframe::Result<()> {
-    let options = eframe::NativeOptions::default();
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_min_inner_size((400.0, 200.0)),
+        ..Default::default()
+    };
+
     eframe::run_native(
-        "My egui App",
+        "IRON CODER",
         options,
         Box::new(|_cc| Ok(Box::<MainWindow>::default())),
     )
